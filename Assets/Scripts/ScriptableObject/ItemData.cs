b13930e405c4ea79702d67012aaf10c5ca34d0d7
jochn96 <whtjdcks55@naver.com>
2025-05-23 12:44:20 +0900

    feat : 아이탬 스크립트 추가

---
+++ Assets/Scripts/ScriptableObject/ItemData.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ItemType
+{
+    Equipable,
+    Consumable
+}
+
+public enum ConsumableType
+{
+    Health
+}
+
+[Serializable]
+public class ItemDataCounsumbale
+{
+    public ConsumableType type;
+    public float value;
+}
+
+[CreateAssetMenu(fileName = "Item", menuName = "New Item")]
+public class ItemData : ScriptableObject
+{
+    [Header("Info")]
+    public string displayName;
+    public string description;
+    public ItemType type;
+    public Sprite icon;
+    public GameObject dropPrefab;
+
+    [Header("Stacking")]
+    public bool canStack;
+    public int maxStackamount;
+
+    [Header("Consumable")]
+    public ItemDataCounsumbale[] counsumbales;
+
+}
